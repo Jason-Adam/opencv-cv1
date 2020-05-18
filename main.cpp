605cbb2903e586e10bb36e26d5661e0d4678bdf4
Jason Adam <jason.adam@aptiveresources.com>
2020-05-18 16:13:27 -0700

    removed buggy function

--- main.cpp
+++ main.cpp
@@ -4,50 +4,25 @@
 using namespace cv;
 using namespace std;
 
-string type2str(int type) {
-    string r;
-    uchar depth = type & CV_MAT_DEPTH_MASK;
-    uchar chans = 1 + (type >> CV_CN_SHIFT);
-
-    switch (depth) {
-        case CV_8U:
-            r = "8U";
-            break;
-        case CV_8S:
-            r = "8S";
-            break;
-        case CV_16U:
-            r = "16U";
-            break;
-        case CV_16S:
-            r = "16S";
-            break;
-        case CV_32S:
-            r = "32S";
-            break;
-        case CV_32F:
-            r = "32F";
-            break;
-        case CV_64F:
-            r = "64F";
-            break;
-        default:
-            r = "User";
-            break;
-    }
-
-    r += "C";
-    r += chans + "0";
-
-    return r;
-}
-
 int main() {
+    // Read in image
     string imagePath = "/Users/jasonadam/github/opencv-cv1/sample_dog.png";
     Mat image = imread(imagePath, IMREAD_COLOR);
-    cout << "Data Type = " << type2str(image.type()) << endl;
-    cout << "Image Dimensions = " << image.size();
-    imshow("dog", image);
+    Mat channel[3];
+    // Print Image metadata
+    cout << "image dimensions = " << image.size() << endl;
+    cout << "number of channels =" << image.channels();
+    // Create Windows
+    namedWindow("Red", 1);
+    namedWindow("Green", 1);
+    namedWindow("Blue", 1);
+    // Split
+    split(image, channel);
+    // Display
+    imshow("Blue", channel[0]);
+    imshow("Green", channel[1]);
+    imshow("Red", channel[2]);
+    // Cleanup
     waitKey(0);
     destroyAllWindows();
 }
