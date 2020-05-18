8444aaa9f3adb4b0769131776e07a3510319b16b
Jason Adam <jason.adam@aptiveresources.com>
2020-05-18 16:15:14 -0700

    spacing update

--- main.cpp
+++ main.cpp
@@ -9,19 +9,24 @@ int main() {
     string imagePath = "/Users/jasonadam/github/opencv-cv1/sample_dog.png";
     Mat image = imread(imagePath, IMREAD_COLOR);
     Mat channel[3];
+
     // Print Image metadata
     cout << "image dimensions = " << image.size() << endl;
     cout << "number of channels =" << image.channels();
+
     // Create Windows
     namedWindow("Red", 1);
     namedWindow("Green", 1);
     namedWindow("Blue", 1);
+
     // Split
     split(image, channel);
+
     // Display
     imshow("Blue", channel[0]);
     imshow("Green", channel[1]);
     imshow("Red", channel[2]);
+
     // Cleanup
     waitKey(0);
     destroyAllWindows();
