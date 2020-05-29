2a449a3babdba221ead0c47d83bba71b5b15d5b6
Jason Adam <22adamj22@gmail.com>
2020-05-29 13:44:51 -0700

    minor updates to gitignore

--- src/week_one/01_getting_started.cpp
+++ src/week_one/01_getting_started.cpp
@@ -5,30 +5,30 @@
 using namespace cv;
 using namespace std;
 
-int main() {
-    // Read in image
-    string imagePath = "/Users/jasonadam/github/opencv-cv1/sample_dog.png";
-    Mat image = imread(imagePath, IMREAD_COLOR);
-    Mat channel[3];
+int weekOne() {
+  // Read in image
+  string imagePath = "/Users/jasonadam/github/opencv-cv1/sample_dog.png";
+  Mat image = imread(imagePath, IMREAD_COLOR);
+  Mat channel[3];
 
-    // Print Image metadata
-    cout << "image dimensions = " << image.size() << endl;
-    cout << "number of channels =" << image.channels();
+  // Print Image metadata
+  cout << "image dimensions = " << image.size() << endl;
+  cout << "number of channels =" << image.channels();
 
-    // Create Windows
-    namedWindow("Red", 1);
-    namedWindow("Green", 1);
-    namedWindow("Blue", 1);
+  // Create Windows
+  namedWindow("Red", 1);
+  namedWindow("Green", 1);
+  namedWindow("Blue", 1);
 
-    // Split
-    split(image, channel);
+  // Split
+  split(image, channel);
 
-    // Display
-    imshow("Blue", channel[0]);
-    imshow("Green", channel[1]);
-    imshow("Red", channel[2]);
+  // Display
+  imshow("Blue", channel[0]);
+  imshow("Green", channel[1]);
+  imshow("Red", channel[2]);
 
-    // Cleanup
-    waitKey(0);
-    destroyAllWindows();
+  // Cleanup
+  waitKey(0);
+  destroyAllWindows();
 }
