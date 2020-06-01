09e36f4ed33ca56d3f0a32b21deffa5b88e8e9e9
Jason Adam <22adamj22@gmail.com>
2020-06-01 09:04:01 -0700

    removed bazel; added cmake to lesson1 folder

--- week1/lesson1/main/getting-started.cpp
+++ week1/lesson1/main/getting-started.cpp
@@ -1,13 +1,13 @@
 // Getting Started
 #include <iostream>
-#include <opencv4/opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace cv;
 using namespace std;
 
 int main() {
   // Read in image
-  string imagePath = "/Users/jasonadam/code/opencv-cv1/imgs/sample_dog.png";
+  string imagePath = "/Users/jasonadam/github/opencv-cv1/imgs/sample_dog.png";
   Mat image = imread(imagePath, IMREAD_COLOR);
   Mat channel[3];
 
--- week1/lesson1/opencv.BUILD
+++
@@ -1,8 +0,0 @@
-cc_library(
-    name = "opencv",
-    srcs = glob(["lib/*.dylib"]),
-    hdrs = glob(["include/opencv4/opencv2/**/*.h*"]),
-    includes = ["include/opencv4"],
-    visibility = ["//visibility:public"],
-    linkstatic = 1,
-)
