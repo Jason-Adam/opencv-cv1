8e7b091dd23e133d3141e901638efd812d22b945
Jason Adam <22adamj22@gmail.com>
2020-06-01 21:16:34 -0700

    updated week1-lesson2 files

--- week1/lesson2/README.md
+++ week1/lesson2/README.md
@@ -1,1 +1,31 @@
+# Running the code  
+You can run the code in the main folder by executing the shell script `runProgram.sh` from this directory:  
+
+```bash  
+sh runProgram.sh
+```  
+
+# Clangd Setup  
+Run the following command in this folder to create a `compile_commands.json` file. This will enable code completion and intellisense for external libraries (opencv in this case):  
+
+```bash  
+cmake -D CMAKE_EXPORT_COMPILE_COMMANDS=1
+```  
+
+The resulting file should look something like this:  
+
+```json  
+[
+  {
+    "directory": "/Users/jasonadam/github/opencv-cv1/week1/lesson2",
+    "command": "/Library/Developer/CommandLineTools/usr/bin/c++   -isystem /usr/local/Cellar/opencv/4.3.0_2/include/opencv4   -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk   -std=gnu++17 -o CMakeFiles/lesson2.dir/main/basic-image-operations.cpp.o -c /Users/jasonadam/github/opencv-cv1/week1/lesson2/main/basic-image-operations.cpp",
+    "file": "/Users/jasonadam/github/opencv-cv1/week1/lesson2/main/basic-image-operations.cpp"
+  }
+]
+```
+
 # Lesson 2 - Basic Image Operations
+* Create new images  
+* Cropping an image section  
+* Copying a region to another  
+* Creating an image mask  
---
+++ week1/lesson2/main/basic-image-operations.cpp
@@ -0,0 +1,44 @@
+// Basic Image Operations
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+  // Read image
+  string imagePath = "/Users/jasonadam/github/opencv-cv1/imgs/sample_dog.png";
+  Mat image = cv::imread(imagePath, IMREAD_COLOR);
+  std::cout << "Standard image size = " << image.size() << endl;
+
+  // Cropped Image
+  Mat croppedImage = image(Range(40, 200), Range(170, 320));
+  namedWindow("normal");
+  namedWindow("cropped");
+  cv::imshow("normal", image);
+  cv::imshow("cropped", croppedImage);
+
+  // Resize Image
+  namedWindow("resized");
+  int resizeDownWidth = 300;
+  int resizeDownHeight = 200;
+  Mat resizedDown;
+  cv::resize(image, resizedDown, Size(resizeDownWidth, resizeDownHeight),
+             INTER_LINEAR);
+  cv::imshow("resized", resizedDown);
+
+  // Resize with scaling factor
+  namedWindow("scaleFactor");
+  double scaleUpX = 1.5;
+  double scaleUpY = 1.5;
+  Mat scaleUp;
+  cv::resize(image, scaleUp, Size(), scaleUpX, scaleUpY, INTER_LINEAR);
+  cv::imshow("scaleFactor", scaleUp);
+  std::cout << "Scaled up image size = " << scaleUp.size() << endl;
+
+  // Cleanup
+  waitKey(0);
+  destroyAllWindows();
+  return 0;
+}
